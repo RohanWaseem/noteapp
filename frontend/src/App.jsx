6151597b850077ccdbc87123f7ee396add88c2dd
Rohan Waseem <rohanwaseem985@gmail.com>
2026-05-05 11:46:33 +0500

    correct name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from './Component/logo'
+import Logo from './Component/Logo'
 
 import {BrowserRouter , Routes , Route}  from "react-router-dom"
 import Createfirstpage from './Component/Createfirstpage'
