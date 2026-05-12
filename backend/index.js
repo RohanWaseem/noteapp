6e79c22e8eaa214078eadc7efbfd183048d9600f
Rohan Waseem <rohanwaseem985@gmail.com>
2026-05-12 10:48:55 +0500

    now

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const API = "https://noteapp-pied.vercel.app"
+
 app.post("/chat", async (req, res) => {
     try {
-        const response = await axios.post("http://localhost:5001/chat", {
+        const response = await axios.post(`${API}/chat`, {
             message: req.body.message
         });
 
